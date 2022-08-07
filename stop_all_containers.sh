90e81b26479fc63d15b554b098e3b20269d21f0d
Veronello <facu2000veronelli@outlook.com>
2022-08-07 19:48:22 -0300

    stop all containers by id

---
+++ stop_all_containers.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+main(){
+    dockerContainers=$(docker ps --format {{.ID}})
+    getConatiners dockerContainers 
+    echo "
+Containers ID:$ids
+Are you sure to stop all of this containers? y/N"
+    read option
+    
+    selectOption
+    
+}
+
+selectOption(){
+    case $option in
+        y | Y)
+            command="docker stop $ids"
+            echo "Stoping containers..."
+            $command
+        ;;
+        n | N | "")
+            echo "Ok. bye bye"
+        ;;
+    esac
+}
+
+getConatiners(){
+    containers=($dockerContainers)
+    LENGTH=${#containers[@]}
+    ids=""
+    if (( $to < 0 || $from < 0 || $to >= $LENGTH || $from >= $LENGTH )); then
+        echo "Invalid data"
+        exit 0
+    fi
+
+    if (( $to == 0 )); then
+        to=$LENGTH
+    fi
+
+    for ((index=$from; index<$to; index++))
+    do
+        ids+=" ${containers[index]}"
+    done
+}
+
+from=0
+to=0
+while getopts t:f: OPTION; do
+    case "$OPTION" in
+        t)
+            to=$OPTARG
+        ;;
+        f)
+            from=$OPTARG
+        ;;
+        ?)
+            echo "script usage: $(basename \$0) [-l] [-h] [-a somevalue]" >&2
+            exit 1
+        ;;
+    esac
+done
+main from to
